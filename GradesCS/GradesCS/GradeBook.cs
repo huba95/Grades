baaf69fea176833312420b6c37438d0772c80bd9
Huttman Balázs <huba95@hotmail.com>
2019-05-18 00:10:44 +0200

    Program created

---
+++ GradesCS/GradesCS/GradeBook.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradesCS
+{
+    class GradeBook
+    {
+        public void AddGrade(float grade)
+        {
+            grades.Add(grade);                  //method
+        }
+
+        List<float> grades;                 //field
+    }
+}
