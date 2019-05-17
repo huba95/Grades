192a1d1f76c6af232ed26cdd6ce8061b0d11e5dc
Huttman Balázs <huba95@hotmail.com>
2019-05-18 01:08:26 +0200

    statistics created

--- GradesCS/GradesCS/GradeBook.cs
+++ GradesCS/GradesCS/GradeBook.cs
@@ -18,6 +18,26 @@ namespace GradesCS
             grades.Add(grade);                  //method
         }
 
+        public GradeStatistics ComputeStatistics()
+        {
+            GradeStatistics stats = new GradeStatistics();
+            
+            float sum = 0;
+            foreach(float grade in grades)
+            {
+                /*if(grade>stats.HighestGrade)
+                {
+                    stats.HighestGrade = grade;
+                }*/
+                stats.HighestGrade = Math.Max(stats.HighestGrade, grade);
+                stats.LowestGrade = Math.Min(stats.LowestGrade, grade);
+                sum = sum + grade;
+            }
+            stats.AverageGrade = sum / grades.Count;
+
+            return stats;
+        }
+
         private List<float> grades;                 //field
     }
 }
--- GradesCS/GradesCS/Program.cs
+++ GradesCS/GradesCS/Program.cs
@@ -13,10 +13,16 @@ namespace GradesCS
             GradeBook book = new GradeBook();
             book.AddGrade(91);
             book.AddGrade(89.5f);
+            book.AddGrade(75);
+            book.AddGrade(99);
 
-            GradeBook book2 = book;
-            book2.AddGrade(95);
-
+            /*  GradeBook book2 = book;
+              book2.AddGrade(95);*/
+            GradeStatistics stats = book.ComputeStatistics();
+            Console.WriteLine(stats.AverageGrade);
+            Console.WriteLine(stats.HighestGrade);
+            Console.WriteLine(stats.LowestGrade);
+            Console.ReadKey();            
         }
     }
 }
